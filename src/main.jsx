26d1a2bb42466182c90542aa3c2e2e2e79ab895a
pioggia <joseph.pioggia@gmail.com>
2026-07-04 23:07:00 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './i18n' // Initialisation i18next — doit être importé avant App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
